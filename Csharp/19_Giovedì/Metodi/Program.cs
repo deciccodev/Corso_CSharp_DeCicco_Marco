8e92917c3ef32fc3d8a8c91e9d825f43e9b5e7de
Nambon03 <deciccodev@gmail.com>
2026-02-19 13:06:47 +0100

    Prova metodi
    UPDATE:
    + aggiunta cartella metodi

---
+++ Csharp/19_Giovedì/Metodi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Metodi
+{
+    static string nome = "Marco";
+    public static void Main(string[] args)
+    {
+        Saluta(nome);        
+        
+        Console.Write("inserisci un numero: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("inserisci un numero: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.Write($"la somma tra {n1} e {n2} è uguale a: {Somma(n1, n2)}");
+    }
+
+    public static int Somma(int a, int b)
+    {
+        int risultato = a + b;
+        return risultato;
+    }
+
+    public static void Saluta(string nome)
+    {
+        Console.Write($"Buongiorno {nome}, come va?\n");
+    }
+}
